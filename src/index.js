ff89875c1a3c29aac54a4c9d7e2e74888c5a00fc
ggt-kun <33932280+ggt-kun@users.noreply.github.com>
2020-11-08 12:35:20 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,55 @@
+const { app, BrowserWindow,ipcMain } = require('electron');
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
+
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 500,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+};
+
+ipcMain.on('form-submission', function (event, name) {
+  console.log("Database Updated");
+});
+
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
---
+++ src/render.js
@@ -0,0 +1,83 @@
+
+let nameObjects = []; 
+const nameInput = document.querySelector('.nameInput');
+const nameDatalist = document.querySelector('#nameDatalist');
+const mysql = require('mysql');
+const ipcRenderer = require('electron').ipcRenderer;
+
+const dbConnection1  = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "form_prototype"
+})
+
+
+dbConnection1.connect((err)=> {
+    if (err){
+        return console.log(err.stack);
+    }
+    console.log("Reading Database...")
+})
+
+let queryString="SELECT * FROM `Employees`";
+dbConnection1.query(queryString,(err, rows, fields) =>{
+    if (err){
+        return console.log("An error occured with the query", err);
+    }
+
+    nameObjects = rows;
+    console.log(nameObjects);
+})
+
+dbConnection1.end(()=> console.log("Connection closed"));
+
+
+nameInput.addEventListener('keypress', function(){
+  let input = nameInput.value;
+  nameDatalist.innerHTML = '';
+  
+  suggestions = nameObjects.filter((nameObject) => {
+      return nameObject.employee_name.toLowerCase().startsWith(input)||nameObject.employee_name.startsWith(input);
+  })
+  console.log(suggestions);
+  suggestions.forEach(element => {
+      let div = document.createElement('option');
+      div.innerHTML = element.employee_name;
+      nameDatalist.appendChild(div);
+  });
+})
+
+//document.getElementById("form").onsubmit = sendForm;
+
+function sendForm(that,event) {
+    event.preventDefault();
+    const dbConnection2  = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "form_prototype"
+    })
+    dbConnection2.connect((err)=> {
+        if (err){
+            return console.log(err.stack);
+        }
+        console.log("Reading Database...")
+    })
+    
+    let queryString=`INSERT INTO Issues (employee_id,date,issue) VALUES ('00000','${that.date.value}','${that.issue.value}');`;
+    dbConnection2.query(queryString,(err, rows, fields) => {
+        if (err){
+            return console.log("An error occured with the query", err);
+        }
+    
+    })
+    
+    dbConnection2.end(()=> console.log("Connection closed"));
+    
+    that.reset();
+    //let name = that.name.value;
+    //ipcRenderer.send('form-submission', name);
+
+}
+
